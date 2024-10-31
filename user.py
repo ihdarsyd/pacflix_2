8aba255091e5e0c18be085684608f7ab51c9c104
ihda <rosy2pacmannai.com>
2024-10-31 21:49:26 +0700

    func subs plan

--- user.py
+++ user.py
@@ -10,6 +10,7 @@ class User:
         5: ["Shania", "Basic Plan", 6, 'shania-123']
     }
 
+
     # Data list_plan
     list_plan = ["Basic Plan", "Standard Plan", "Premium Plan"]
     list_benefit = [[True, True, True, "Bisa Stream"],
@@ -19,8 +20,8 @@ class User:
                  [False, False, True, "Kualitas UHD"],
                  [1, 2, 4, "Number of Devices"],
                  ["3rd party Movie only", "Basic Plan Content + Sports", "Basic Plan + Standard Plan + PacFlix Original Series", "Jenis Konten"],
-                 [120_000, 160_000, 200_000, "Harga"]]
-    headers =  ["Basic Plan", "Standard Plan", "Premium Plan", "Services"]
+                 [120_000, 160_000, 200_000, "Harga"]] # data
+    headers =  ["Basic Plan", "Standard Plan", "Premium Plan", "Services"] # header atau nama kolom
 
     
     
@@ -102,3 +103,46 @@ class User:
             print("New Plan tidak tersedia")
         elif(self.current_plan is None):
             print("Silakan berlangganan terlebih dahulu")       
+
+
+    def subs_plan(self, new_plan, kode_referral):
+        """
+        Fungsi untuk melakukan subscribe plan bagi user baru
+
+        input:
+            - new_plan (str)
+            - kode_referral (str)
+        """
+        list_code = [x[-1] for x in self.data_user.values()]
+        # list_code = []
+
+        # for x in self.data_user.values():
+        #     list_code.append(x[-1])
+
+        if(self.current_plan is None):
+
+            if(new_plan in self.list_plan):
+                # do subs
+                self.current_plan = new_plan
+                self.duration_plan = 1
+                self.kode_referral =  f"{self.username}-123"
+                
+                idx_new_plan = self.list_plan.index(new_plan)
+                #menampilkan harganya
+                if(kode_referral in list_code):
+                    # dapat diskon
+                    total = self.list_benefit[-1][idx_new_plan] - (self.list_benefit[-1][idx_new_plan] * 0.04)
+                    
+                else:
+                    # harga normal
+                    total = self.list_benefit[-1][idx_new_plan]
+                print(f"Harga yang harus dibayarkan untuk subs {new_plan} adalah Rp.{total}")
+
+                # Tambahakan user baru
+                last_key = max(self.data_user.keys())
+                self.data_user[last_key+1] = [self.username,self.current_plan,self.duration_plan,self.kode_referral]
+
+            else:
+                print("Plan tidak Tersedia")
+        else:
+            print("Anda sudah memiliki plan")
